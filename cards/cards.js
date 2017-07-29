c19411b9cd2ba7109c41c9444a124a98fbb5aa37
Austin F <austindanielfrench@gmail.com>
2017-07-28 21:29:41 -0700

    IDK WHATS GOING ON

--- cards/cards.js
+++ cards/cards.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 var deckID = "cbw7wukkqho9";
 console.log(deckID);
 var shuffle = "https://deckofcardsapi.com/api/deck/"+deckID+"/shuffle/";
@@ -12,21 +13,27 @@ var code;
 var value;
 
 // drawing a card json reader
+
+
 $.getJSON(drawCard, function(data) {
     image = data.cards[0].image;
     suit = data.cards[0].suit;
     code = data.cards[0].code;
     value = data.cards[0].value;
+    console.log(image, suit, code, value);
 });
-console.log(image, suit, code, value);
+	
+	$('#drawCard1').click(function(){
+		document.getElementById("imageDisplay1").innerHTML = "<img src='" + image + "'>"
+});
+	$('#drawCard2').click(function(){
+		
+	});
+	$('#drawCard3').click(function(){
+		
+	});
+	$('#shuffleButton').click(function(){
+		
+	});
 
-// var card = [
-//     {
-
-//         image: drawCard.cards.image,
-//         suit: drawCard.cards.suit,
-//         code: drawCard.cards.code,
-//         value: drawCard.cards.value
-
-//     }
-// ]
+});
