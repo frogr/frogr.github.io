ed0eb6b2518f5a3e5dfdfa05f6316243a27f10e3
Austin F <austindanielfrench@gmail.com>
2018-02-11 22:26:51 -0800

    header + installed react router

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+const Header = () => {
+  return (
+    <div className="headerContainer">
+      <h1>austin "frog" french</h1>
+      <hr />
+      <a href="#">about me</a> - <a href="#">blog</a> - <a href="#">projects</a>{' '}
+      - <a href="#">contact</a>
+    </div>
+  );
+};
+
+export default Header;
