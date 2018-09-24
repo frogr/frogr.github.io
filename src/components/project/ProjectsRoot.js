8485d749a990c5ce7818065d96454236122faf2e
frogr <austindanielfrench@gmail.com>
2018-09-24 05:14:27 -0700

    added manual projects instead of routing through json

--- src/components/project/ProjectsRoot.js
+++ src/components/project/ProjectsRoot.js
@@ -4,19 +4,92 @@ import { Link } from 'react-router-dom';
 
 const Project = () => {
   return (
-    <div className="textContent">
-      {ProjectContent.slice(0)
-        .map(project => {
-          return (
-            <div key={project.id}>
-              <h2>
-                <Link to={'/projects/' + project.id}>{project.title}</Link>
-              </h2>
-              <p>{project.date}</p>
-              <p>{project.blurb}</p>
-            </div>
-          );
-        })}
+    <div className="projectContainer">
+      <div className="textContent">
+        <div className="projectHeader">
+          These are some of my recent projects.
+        </div>
+        <div className="project">
+          <div className="projectTitle"> Backwoods </div>
+          <div className="projectLink"> Live Link </div>
+          <div className="projectLink"> Github Link </div>
+          <div className="projectDescription"> This is some desc text. </div>
+          <div className="projectTechs">
+            {' '}
+            These are the technologies I used in the application{' '}
+          </div>
+        </div>
+        <div className="project">
+          <div className="projectTitle"> Car Searching </div>
+          <div className="projectLink"> Live Link </div>
+          <div className="projectLink"> Github Link </div>
+          <div className="projectDescription"> This is some desc text. </div>
+          <div className="projectTechs">
+            {' '}
+            These are the technologies I used in the application{' '}
+          </div>
+        </div>
+        <div className="project">
+          <div className="projectTitle"> Ribbit Ribbit </div>
+          <div className="projectLink"> Live Link </div>
+          <div className="projectLink"> Github Link </div>
+          <div className="projectDescription"> This is some desc text. </div>
+          <div className="projectTechs">
+            {' '}
+            These are the technologies I used in the application{' '}
+          </div>
+        </div>
+        <div className="project">
+          <div className="projectTitle"> frogChat </div>
+          <div className="projectLink"> Live Link </div>
+          <div className="projectLink"> Github Link </div>
+          <div className="projectDescription"> This is some desc text. </div>
+          <div className="projectTechs">
+            {' '}
+            These are the technologies I used in the application{' '}
+          </div>
+        </div>
+        <div className="project">
+          <div className="projectTitle"> frog.GG </div>
+          <div className="projectLink"> Live Link </div>
+          <div className="projectLink"> Github Link </div>
+          <div className="projectDescription"> This is some desc text. </div>
+          <div className="projectTechs">
+            {' '}
+            These are the technologies I used in the application{' '}
+          </div>
+        </div>
+        <div className="project">
+          <div className="projectTitle"> Youtube Searching </div>
+          <div className="projectLink"> Live Link </div>
+          <div className="projectLink"> Github Link </div>
+          <div className="projectDescription"> This is some desc text. </div>
+          <div className="projectTechs">
+            {' '}
+            These are the technologies I used in the application{' '}
+          </div>
+        </div>
+        <div className="project">
+          <div className="projectTitle"> Twitch following list </div>
+          <div className="projectLink"> Live Link </div>
+          <div className="projectLink"> Github Link </div>
+          <div className="projectDescription"> This is some desc text. </div>
+          <div className="projectTechs">
+            {' '}
+            These are the technologies I used in the application{' '}
+          </div>
+        </div>
+        <div className="project">
+          <div className="projectTitle"> Wikipedia search </div>
+          <div className="projectLink"> Live Link </div>
+          <div className="projectLink"> Github Link </div>
+          <div className="projectDescription"> This is some desc text. </div>
+          <div className="projectTechs">
+            {' '}
+            These are the technologies I used in the application{' '}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
